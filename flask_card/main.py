c9b69ef67037dc759ebb3694e8386ef049a4ac30
Giang Ta <thoanggiang98@gmail.com>
2020-04-11 22:14:06 -0400

    prototyping dashboard

--- flask_card/main.py
+++ flask_card/main.py
@@ -1,19 +1,17 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 from . import db
+from .models import User
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template("landing.html")
-
-@main.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template("dashboard.html")
+    if not current_user.is_authenticated:
+        return render_template("landing.html")
+    return render_template("dashboard.html", user=current_user)
 
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template("profile.html")
+    return render_template("profile.html", user=current_user)
